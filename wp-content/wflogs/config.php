9071acbd34ec3fc8ba4c220c7df6f95612c4bc98
Kamicron <ludovic.chevroulet@outlook.fr>
2022-12-02 11:02:27 +0100

    Wordfence plugin

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1670579965;s:7:"authKey";s:64:"9Iptzv|2rO7nc roFIAhSV@y~ 5PGv[E;<VL{G5X^u0y[SePG`5,SByjD!2 Qd4b";s:7:"version";s:5:"1.0.5";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:307;}
